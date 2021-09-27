2e144759cdf21d66efe9ea24d6626a8e4eabbdc3
Himanshu Patil <patilhnofficial@gmail.com>
2021-09-27 15:58:21 +0530

    updates

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,15 +1,33 @@
-import React from "react";
-import { View, Text,Button } from "react-native";
-const LandingPage = ({navigation}) => {
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, TextInput } from "react-native";
+const LandingPage = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = () => {
+    navigation.navigate("profile");
+  };
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Landing Page</Text>
       <View>
-    <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('profile')}
-      />
-    </View>
+        <TextInput
+          placeholder="enter your email"
+          editable
+          maxLength={40}
+          style={{ padding: 10 }}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        />
+        <TextInput
+          placeholder="enter your password"
+          editable
+          maxLength={40}
+          style={{ padding: 10 }}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
+        <Button title="Login" onPress={handleLogin} />
+      </View>
     </View>
   );
 };
